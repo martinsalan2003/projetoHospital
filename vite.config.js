4f348a4d42922daf31c1954903ff03c851fa1efc
AlanMartins00 <alanmartinsferreira59@gmail.com>
2024-11-18 19:48:42 -0300

    configuração do vite

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/S-o-Lucas-Mendel",
-  resolve: {
-    alias: {
-      '@assets': '/src/assets', 
-      
-    },
-  },
+  base:"/projetoHospita",
 })
